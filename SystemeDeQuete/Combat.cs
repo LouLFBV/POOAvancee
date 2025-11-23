c39b9ca2fcf4d717f3b763c5ffcd93ce4956a8c2
LouLFBV <lou.lefebvre@ynov.com>
2025-11-23 21:28:13 +0100

    First Commit

---
+++ SystemeDeQuete/Combat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemeDeQuete
+{
+    class Combat : Quete, IPerteDOr
+    {
+        public void VolDOr(int montant)
+        {
+        }
+    }
+}
---
+++ SystemeDeQuete/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SystemeDeQuete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans le système de gestion de quêtes !");
+        }
+    }
+}
