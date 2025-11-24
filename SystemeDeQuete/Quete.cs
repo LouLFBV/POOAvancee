df92f48e64d84849d33ac044ec8faea6e7cc9d90
Hugo Cabanes <hugo33tab@gmail.com>
2025-11-24 11:58:37 +0100

    HugoChad

--- SystemeDeQuete/Quete.cs
+++ SystemeDeQuete/Quete.cs
@@ -8,6 +8,53 @@ namespace SystemeDeQuete
 {
     class Quete
     {
+        private string _titre;
+        private string _description;
+        private Importance _importance;
+        private Evenement _evenement;
+
+        public Quete(string titre, string description, Importance importance, Evenement evenement)
+        {
+            _titre = titre;
+            _description = description;
+            _importance = importance;
+            _evenement = evenement;
+        }
+
+        public string AfficherTitre()
+        {
+            return _titre;
+        }
+
+        public string AfficherDescription()
+        {
+            return _description;
+        }
+
+        public Importance AfficherImportance()
+        {
+            return _importance;
+        }
+
+        public Evenement AfficherEvenement()
+        {
+            return _evenement;
+        }
+
+        public void ModifierTitre(string title)
+        {
+            _titre = title;
+        }
+
+        public void ModifierDescription(string descrip)
+        {
+            _description = descrip;
+        }
+
+        public void ModifierImportance(Importance important)
+        {
+            _importance = important;
+        }
         public void Test()
         {
             Log.Info("Démarrage OK");
@@ -16,5 +63,11 @@ namespace SystemeDeQuete
             Console.WriteLine("Entrer un chiffre !");
             var chiffre = Console.ReadLine();
         }
+    }
+    public enum Importance
+    {
+        Principale,
+        Secondaire,
+        SousQuete
     }
 }
